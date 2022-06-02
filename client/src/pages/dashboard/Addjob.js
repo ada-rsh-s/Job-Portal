90ac15a5c371469a29f8ba3299672a6395e4945c
Adarsh-S1 <adarshvillasuni@gmail.com>
2022-06-02 17:27:04 +0530

    Basix routing done

---
+++ client/src/pages/dashboard/Addjob.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Addjob = () => {
+  return (
+    <h1>Add Job</h1>
+  )
+}
+
+export default Addjob
---
+++ client/src/pages/dashboard/index.js
@@ -0,0 +1,6 @@
+import Addjob from "./Addjob";
+import Alljobs from "./Alljobs";
+import Profile from "./Profile";
+import Sharedlayout from "./Sharedlayout";
+import Stats from "./Stats";
+export { Addjob, Alljobs, Profile, Stats, Sharedlayout };
