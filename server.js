cb9113d381d186a5e22fb5f7bd70df4471912050
ada-rsh-s <aadarsh04ss@gmail.com>
2025-03-30 14:24:58 +0530

    Produciton ready Keep awake chagne

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 import express from "express";
-
-const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import "express-async-errors";
@@ -9,8 +7,6 @@ import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
-import helmet from "helmet";
-import mongoSanitize from "express-mongo-sanitize";
 
 import connectDB from "./db/connect.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -20,26 +16,33 @@ import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import authenticateUser from "./middleware/auth.js";
 
+const app = express();
+
 if (process.env.NODE_ENV !== "production") {
-  // app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-if (process.env.NODE_ENV === "production") {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-}
 app.use(express.json());
-// app.use(helmet());
-// app.use(mongoSanitize());
+
 
 app.use("/api/v1/auth", authRoutes);
-
 app.use("/api/v1/jobs", authenticateUser, jobsRoutes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build"))); // Serve static files
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// ✅ Error handlers
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -49,7 +52,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
-      console.log(`Server listening ${port}`);
+      console.log(`Server listening on port ${port}`);
     });
   } catch (error) {
     console.log(error);
